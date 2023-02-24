3d9e4d4ad319dc87648cd600e813ea922a8e5bb5
Mathieu Zimeris <alexvargas@hotmail.fr>
2023-02-24 12:51:20 +0100

    ok

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,8 @@ class List < ApplicationRecord
   has_one_attached :image
   attr_accessor :image_url
 
-  has_many :bookmarks;
+  has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
 end
